a8e36f00d202ea2c8d69ad8be0c5be0bc83f4ebe
Ee Qi Lim <143212045+eeqilim@users.noreply.github.com>
2023-12-02 19:19:40 +0000

    Handle onStringSelectInteraction.

--- src/main/java/edu/northeastern/cs5500/starterbot/command/TradeCommand.java
+++ src/main/java/edu/northeastern/cs5500/starterbot/command/TradeCommand.java
@@ -3,7 +3,10 @@ package edu.northeastern.cs5500.starterbot.command;
 import edu.northeastern.cs5500.starterbot.controller.PokedexController;
 import edu.northeastern.cs5500.starterbot.controller.PokemonController;
 import edu.northeastern.cs5500.starterbot.controller.TrainerController;
+import edu.northeastern.cs5500.starterbot.model.Pokemon;
+import edu.northeastern.cs5500.starterbot.model.Trainer;
 import java.util.Collection;
+import java.util.Objects;
 import javax.annotation.Nonnull;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
@@ -14,6 +17,7 @@ import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.interactions.components.selections.StringSelectMenu;
+import org.bson.types.ObjectId;
 
 @Slf4j
 public class TradeCommand implements SlashCommandHandler, StringSelectHandler {
@@ -30,6 +34,8 @@ public class TradeCommand implements SlashCommandHandler, StringSelectHandler {
     public TradeCommand() {
         // Empty and public for Dagger
     }
+
+    private String otherPlayerDiscordId;
 
     @Override
     @Nonnull
@@ -48,15 +54,15 @@ public class TradeCommand implements SlashCommandHandler, StringSelectHandler {
     @Override
     public void onSlashCommandInteraction(@Nonnull SlashCommandInteractionEvent event) {
         log.info("event: /trade");
+        otherPlayerDiscordId = event.getOption("player").getAsMember().getId();
+        String trainerDiscordId = event.getMember().getId();
+        Collection<String> pokemonList =
+                trainerController.getPokemonNamesFromTrainerInventory(trainerDiscordId);
 
         EmbedBuilder embedBuilder = new EmbedBuilder();
         embedBuilder.setTitle("Trade Pokemon with Friend pika pika!");
         embedBuilder.setDescription("Choose a Pokemon from your inventory to trade");
         embedBuilder.setImage("http://media3.giphy.com/media/Zqy8kRlwfwFnq/giphy.gif");
-
-        String trainerDiscordId = event.getMember().getId();
-        Collection<String> pokemonList =
-                trainerController.getPokemonNamesFromTrainerInventory(trainerDiscordId);
 
         if (!pokemonList.isEmpty()) {
             StringSelectMenu.Builder menu =
@@ -69,7 +75,27 @@ public class TradeCommand implements SlashCommandHandler, StringSelectHandler {
             event.replyEmbeds(embedBuilder.build())
                     .addActionRow(menu.build())
                     .setEphemeral(false)
-                    .queue();
+                    .queue(
+                            response -> {
+                                // Provide instructions for the other user to respond in the private
+                                // channel
+                                event.getJDA()
+                                        .retrieveUserById(otherPlayerDiscordId)
+                                        .queue(
+                                                user -> {
+                                                    user.openPrivateChannel()
+                                                            .queue(
+                                                                    privateChannel -> {
+                                                                        privateChannel
+                                                                                .sendMessage(
+                                                                                        String
+                                                                                                .format(
+                                                                                                        "You received a trade offer from Player <@%s>! Respond now!",
+                                                                                                        trainerDiscordId))
+                                                                                .queue();
+                                                                    });
+                                                });
+                            });
         } else {
             embedBuilder.setFooter(
                     String.format("There is no Pokemon in your team 👀 Use /catch to get one 🤩"));
@@ -78,5 +104,24 @@ public class TradeCommand implements SlashCommandHandler, StringSelectHandler {
     }
 
     @Override
-    public void onStringSelectInteraction(@Nonnull StringSelectInteractionEvent event) {}
+    public void onStringSelectInteraction(@Nonnull StringSelectInteractionEvent event) {
+        String trainerDiscordId = event.getMember().getId();
+        final String response = event.getInteraction().getValues().get(0);
+        Objects.requireNonNull(response);
+
+        Trainer trainer = trainerController.getTrainerForMemberId(trainerDiscordId);
+        ObjectId pokemonId =
+                trainerController.getPokemonIdByPokemonName(trainerDiscordId, response);
+        Pokemon pokemon = pokemonController.getPokemonById(pokemonId.toString());
+        String pokemonName =
+                pokedexController.getPokemonSpeciesByNumber(pokemon.getPokedexNumber()).getName();
+
+        //     tradeOfferController.createNewOffering(trainer, pokemon);
+
+        event.reply(
+                        String.format(
+                                "Trade offer for Pokemon %s successfully created! Wait for your friend to respond!",
+                                pokemonName))
+                .queue();
+    }
 }
