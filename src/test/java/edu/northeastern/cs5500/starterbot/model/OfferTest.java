979c2d63585262881f71d766402b67d34c825ab1
Ee Qi Lim <143212045+eeqilim@users.noreply.github.com>
2023-12-06 06:08:02 +0000

    Test model classes.

--- src/test/java/edu/northeastern/cs5500/starterbot/model/OfferTest.java
+++ src/test/java/edu/northeastern/cs5500/starterbot/model/OfferTest.java
@@ -1,22 +1,91 @@
 package edu.northeastern.cs5500.starterbot.model;
 
 import static com.google.common.truth.Truth.assertThat;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
 
 public class OfferTest {
+    final String trainerId = "trainer1";
+    final String otherTrainerId = "trainer2";
+    final String newTrainerId = "trainer3";
+    final String newOtherTrainerId = "trainer4";
+
+    final String pokemonId = "pokemon1";
+    final String otherPokemonId = "pokemon2";
+    final String newPokemonId = "pokemon3";
+    final String newOtherPokemonId = "pokemon4";
+
     @Test
     void testOfferCreation() {
-        String trainerId = "trainer1";
-        String otherTrainerId = "trainer2";
-        String pokemonId = "pokemon1";
-        String otherPokemonId = "pokemon2";
-
         Offer offer = new Offer(trainerId, otherTrainerId, pokemonId, otherPokemonId);
 
         assertThat(offer.getTrainerId()).isEqualTo(trainerId);
         assertThat(offer.getOtherTrainerId()).isEqualTo(otherTrainerId);
         assertThat(offer.getPokemonId()).isEqualTo(pokemonId);
         assertThat(offer.getOtherPokemonId()).isEqualTo(otherPokemonId);
+    }
+
+    @Test
+    void testOfferSetters() {
+        Offer offer = new Offer(trainerId, otherTrainerId, pokemonId, otherPokemonId);
+
+        offer.setTrainerId(newTrainerId);
+        offer.setOtherTrainerId(newOtherTrainerId);
+        offer.setPokemonId(newPokemonId);
+        offer.setOtherPokemonId(newOtherPokemonId);
+
+        assertThat(offer.getTrainerId()).isEqualTo(newTrainerId);
+        assertThat(offer.getOtherTrainerId()).isEqualTo(newOtherTrainerId);
+        assertThat(offer.getPokemonId()).isEqualTo(newPokemonId);
+        assertThat(offer.getOtherPokemonId()).isEqualTo(newOtherPokemonId);
+    }
+
+    @Test
+    void testSetTrainerIdNullValue() {
+        Offer offer = new Offer("trainer1", "trainer2", "pokemon1", "pokemon2");
+
+        try {
+            offer.setTrainerId(null);
+            fail("Expected NullPointerException");
+        } catch (NullPointerException e) {
+            assertThat(e.getMessage()).isEqualTo("trainerId is marked non-null but is null");
+        }
+    }
+
+    @Test
+    void testSetOtherTrainerIdNullValue() {
+        Offer offer = new Offer("trainer1", "trainer2", "pokemon1", "pokemon2");
+
+        try {
+            offer.setOtherTrainerId(null);
+            fail("Expected NullPointerException");
+        } catch (NullPointerException e) {
+            assertThat(e.getMessage()).isEqualTo("otherTrainerId is marked non-null but is null");
+        }
+    }
+
+    @Test
+    void testSetPokemonIdNullValue() {
+        Offer offer = new Offer("trainer1", "trainer2", "pokemon1", "pokemon2");
+
+        try {
+            offer.setPokemonId(null);
+            fail("Expected NullPointerException");
+        } catch (NullPointerException e) {
+            assertThat(e.getMessage()).isEqualTo("pokemonId is marked non-null but is null");
+        }
+    }
+
+    @Test
+    void testSetOtherPokemonIdNullValue() {
+        Offer offer = new Offer("trainer1", "trainer2", "pokemon1", "pokemon2");
+
+        try {
+            offer.setOtherPokemonId(null);
+            fail("Expected NullPointerException");
+        } catch (NullPointerException e) {
+            assertThat(e.getMessage()).isEqualTo("otherPokemonId is marked non-null but is null");
+        }
     }
 }
--- src/test/java/edu/northeastern/cs5500/starterbot/model/PokemonTest.java
+++ src/test/java/edu/northeastern/cs5500/starterbot/model/PokemonTest.java
@@ -1,25 +1,33 @@
 package edu.northeastern.cs5500.starterbot.model;
 
 import static com.google.common.truth.Truth.assertThat;
+import static org.junit.jupiter.api.Assertions.fail;
 
+import org.bson.types.ObjectId;
 import org.junit.jupiter.api.Test;
 
 public class PokemonTest {
+    ObjectId pokemonId = new ObjectId();
+
+    Pokemon pokemon =
+            Pokemon.builder()
+                    .pokedexNumber(1)
+                    .hp(100)
+                    .attack(50)
+                    .defense(30)
+                    .specialAttack(40)
+                    .specialDefense(30)
+                    .speed(60)
+                    .total(250)
+                    .build();
+
     @Test
     void testPokemonBuilder() {
-        Pokemon pokemon =
-                Pokemon.builder()
-                        .pokedexNumber(1)
-                        .hp(100)
-                        .attack(50)
-                        .defense(30)
-                        .specialAttack(40)
-                        .specialDefense(30)
-                        .speed(60)
-                        .total(250)
-                        .build();
+        pokemon.setId(pokemonId);
+        pokemon.setPokedexNumber(10);
 
-        assertThat(pokemon.getPokedexNumber()).isEqualTo(1);
+        assertThat(pokemon.getId()).isEqualTo(pokemonId);
+        assertThat(pokemon.getPokedexNumber()).isEqualTo(10);
         // Default Value
         assertThat(pokemon.getLevel()).isEqualTo(0);
         assertThat(pokemon.getMood()).isEqualTo(0);
@@ -30,5 +38,95 @@ public class PokemonTest {
         assertThat(pokemon.getSpecialDefense()).isEqualTo(30);
         assertThat(pokemon.getSpeed()).isEqualTo(60);
         assertThat(pokemon.getTotal()).isEqualTo(250);
+    }
+
+    @Test
+    void testSetPokedexNumberNullValue() {
+        try {
+            pokemon.setPokedexNumber(null);
+            fail("Expected NullPointerException");
+        } catch (NullPointerException e) {
+            assertThat(e.getMessage()).isEqualTo("pokedexNumber is marked non-null but is null");
+        }
+    }
+
+    @Test
+    void testSetLevelNullValue() {
+        try {
+            pokemon.setLevel(null);
+            fail("Expected NullPointerException");
+        } catch (NullPointerException e) {
+            assertThat(e.getMessage()).isEqualTo("level is marked non-null but is null");
+        }
+    }
+
+    @Test
+    void testSetMoodNullValue() {
+        try {
+            pokemon.setMood(null);
+            fail("Expected NullPointerException");
+        } catch (NullPointerException e) {
+            assertThat(e.getMessage()).isEqualTo("mood is marked non-null but is null");
+        }
+    }
+
+    @Test
+    void testSetHpNullValues() {
+        try {
+            pokemon.setHp(null);
+            fail("Expected NullPointerException");
+        } catch (NullPointerException e) {
+            assertThat(e.getMessage()).isEqualTo("hp is marked non-null but is null");
+        }
+    }
+
+    @Test
+    void testSetAttackNullValue() {
+        try {
+            pokemon.setAttack(null);
+            fail("Expected NullPointerException");
+        } catch (NullPointerException e) {
+            assertThat(e.getMessage()).isEqualTo("attack is marked non-null but is null");
+        }
+    }
+
+    @Test
+    void testSetSpecialAttackNullValue() {
+        try {
+            pokemon.setSpecialAttack(null);
+            fail("Expected NullPointerException");
+        } catch (NullPointerException e) {
+            assertThat(e.getMessage()).isEqualTo("specialAttack is marked non-null but is null");
+        }
+    }
+
+    @Test
+    void testSetSpecialDefenseNullValue() {
+        try {
+            pokemon.setSpecialDefense(null);
+            fail("Expected NullPointerException");
+        } catch (NullPointerException e) {
+            assertThat(e.getMessage()).isEqualTo("specialDefense is marked non-null but is null");
+        }
+    }
+
+    @Test
+    void testSetSpeedNullValue() {
+        try {
+            pokemon.setSpeed(null);
+            fail("Expected NullPointerException");
+        } catch (NullPointerException e) {
+            assertThat(e.getMessage()).isEqualTo("speed is marked non-null but is null");
+        }
+    }
+
+    @Test
+    void testSetTotaldNullValue() {
+        try {
+            pokemon.setTotal(null);
+            fail("Expected NullPointerException");
+        } catch (NullPointerException e) {
+            assertThat(e.getMessage()).isEqualTo("total is marked non-null but is null");
+        }
     }
 }
